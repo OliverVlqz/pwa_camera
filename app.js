7579211cc3942d89bf3b2b3206afc6707ed3e093
OliverVlqz <oliveratom772@gmail.com>
2025-12-14 19:18:51 -0600

    fix: correct service worker registration path and script tag syntax in index.html

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 navigator.serviceWorker
-  .register('sw.js')
+  .register('./sw.js')
   .then((registration) => {
     console.log('Service Worker registered with scope:', registration.scope)
   })
